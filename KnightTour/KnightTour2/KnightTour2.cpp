74ebe64798da09ee00d08589faf0b608ee82a5b4
vivisuke <ntsuda@master.email.ne.jp>
2021-01-08 12:50:05 +0900

    枝刈り無し版 追加

--- KnightTour/KnightTour2/KnightTour2.cpp
+++ KnightTour/KnightTour2/KnightTour2.cpp
@@ -8,9 +8,100 @@ typedef unsigned char uchar;
 typedef const int cint;
 #define		WALL		0xff
 
-class Board {
+class Board {	//	枝刈り無し版
 public:
 	Board(int wd = 10, int ht = 3)
+	{
+		setSize(wd, ht);
+	}
+public:
+	inline int xyToIX(int x, int y) const { return (x+2) + (y+2) * m_ARY_WIDTH; }
+	void setSize(int wd, int ht) {
+		m_BD_WIDTH = wd;
+		m_BD_HEIGHT = ht;
+		m_BD_SIZE = wd*ht;
+		m_ARY_WIDTH = wd+4;
+		m_ARY_HEIGHT = ht+4;
+		m_ARY_SIZE = (wd+4) * (ht+4);
+		//	盤面配列初期化
+		m_bd.resize(m_ARY_SIZE);
+		for(auto& x: m_bd) x = WALL;	//	番人
+		for (int y = 0; y < ht; ++y) {
+			for (int x = 0; x < wd; ++x) {
+				m_bd[xyToIX(x, y)] = 0;
+			}
+		}
+		//printBoard();
+	}
+	void printBoard() const {		//	盤面表示
+		for (int y = 0; y < m_BD_HEIGHT; ++y) {
+			for (int x = 0; x < m_BD_WIDTH; ++x) {
+				auto t = to_string((int)m_bd[xyToIX(x, y)]);
+				if( t.size() < 2 ) t = ' ' + t;
+				cout << t << " ";
+			}
+			cout << "\n";
+		}
+		cout << "\n";
+	}
+	void KnightTour(int n, int x, int y) {
+		m_bd[xyToIX(x, y)] = n;
+		//cout << n << ":\n";
+		//printBoard();
+		if( x == 1 && y == 2 ) {
+			if( n == m_BD_SIZE ) {	//	found!
+				++m_count;
+				if (m_count % 1000 == 0) cout << ".";
+				//cout << m_count << ":\n";
+				//printBoard();
+			} else if( n != 2 ) {
+				m_bd[xyToIX(x, y)] = 0;
+				return;
+			}
+		}
+		if (m_bd[xyToIX(x - 1, y - 2)] == 0)
+			KnightTour(n + 1, x - 1, y - 2);
+		if (m_bd[xyToIX(x + 1, y - 2)] == 0)
+			KnightTour(n + 1, x + 1, y - 2);
+		if (m_bd[xyToIX(x - 2, y - 1)] == 0)
+			KnightTour(n + 1, x - 2, y - 1);
+		if (m_bd[xyToIX(x + 2, y - 1)] == 0)
+			KnightTour(n + 1, x + 2, y - 1);
+		if (m_bd[xyToIX(x - 2, y + 1)] == 0)
+			KnightTour(n + 1, x - 2, y + 1);
+		if (m_bd[xyToIX(x + 2, y + 1)] == 0)
+			KnightTour(n + 1, x + 2, y + 1);
+		if (m_bd[xyToIX(x - 1, y + 2)] == 0)
+			KnightTour(n + 1, x - 1, y + 2);
+		if (m_bd[xyToIX(x + 1, y + 2)] == 0)
+			KnightTour(n + 1, x + 1, y + 2);
+		m_bd[xyToIX(x, y)] = 0;
+		//cout << n << "(ret):\n";
+		//printBoard();
+	}
+	int countKnightTour(int wd, int ht) {
+		setSize(wd, ht);
+		//
+		m_count = 0;
+		m_bd[xyToIX(0, 0)] = 1;		//	左上隅から開始
+		//updateNConnect(0, 0, -1);
+		KnightTour(2, 2, 1);		//	最初は (2, 1) に移動
+		return m_count;
+	}
+private:
+	int m_count = 0;
+	int m_BD_WIDTH;		//	盤面幅
+	int m_BD_HEIGHT;	//	盤面高
+	int m_BD_SIZE;
+	int m_ARY_WIDTH;	//	番人を含んだ配列幅
+	int m_ARY_HEIGHT;	//	番人を含んだ配列高
+	int m_ARY_SIZE;
+	vector<uchar>	m_bd;			//	番人を含んだ盤面配列
+	//vector<uchar>	m_nConnect;		//	連結可能数
+};
+class BoardPruning {	//	枝刈り有り版
+public:
+	BoardPruning(int wd = 10, int ht = 3)
 	{
 		setSize(wd, ht);
 	}
